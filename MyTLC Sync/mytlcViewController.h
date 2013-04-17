ab8fc3b311c4bb8d9454ce2c3085137991ba8d29
Devin Collins <agent14709@gmail.com>
2013-04-17 14:11:25 -0500

    Initial commit

---
+++ MyTLC Sync/mytlcViewController.h
@@ -0,0 +1,13 @@
+//
+//  mytlcViewController.h
+//  MyTLC Sync
+//
+//  Created by Devin Collins on 4/17/13.
+//  Copyright (c) 2013 Layer 8 Applications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mytlcViewController : UIViewController
+
+@end
---
+++ MyTLC Sync/mytlcViewController.m
@@ -0,0 +1,29 @@
+//
+//  mytlcViewController.m
+//  MyTLC Sync
+//
+//  Created by Devin Collins on 4/17/13.
+//  Copyright (c) 2013 Layer 8 Applications. All rights reserved.
+//
+
+#import "mytlcViewController.h"
+
+@interface mytlcViewController ()
+
+@end
+
+@implementation mytlcViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
