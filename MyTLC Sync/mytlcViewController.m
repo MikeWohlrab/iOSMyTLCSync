b7967ea85c9590ad4a3248bfe45bb6b62271bd66
Devin Collins <agent14709@gmail.com>
2013-04-24 22:19:16 -0500

    Started working on parsing algorithm

--- MyTLC Sync/mytlcViewController.m
+++ MyTLC Sync/mytlcViewController.m
@@ -9,7 +9,8 @@
 #import "mytlcViewController.h"
 
 @interface mytlcViewController ()
-
+- (NSString*)parseToken:(NSString*)data;
+- (NSString*)getData;
 @end
 
 @implementation mytlcViewController
@@ -17,19 +18,60 @@
 @synthesize txtUsername;
 @synthesize txtPassword;
 
-- (IBAction)buttonPressed {
-    NSString* username = txtUsername.text;
-    NSString* password = txtPassword.text;
-    if ((![username isEqualToString:@""]) && (![password isEqualToString:@""])) {
-        NSLog(@"Username: %@\nPassword: %@", username, password);
-    } else if ((![username isEqualToString:@""]) && ([password isEqualToString:@""])) {
-        NSLog(@"Username: %@", username);
-    } else if (([username isEqualToString:@""]) && (![password isEqualToString:@""])) {
-        NSLog(@"Password: %@", password);
-    } else {
-        NSLog(@"No information");
-    }
+NSString* username;
+NSString* password;
 
+NSString* loginToken;
+
+- (IBAction)buttonPressed {
+    username = txtUsername.text;
+    password = txtPassword.text;
+    [self runEvents];
+}
+
+- (NSString*) runEvents {
+    NSString* tempToken = nil;
+    NSString* securityToken = nil;
+
+    NSString* data = [self getData:@"https://mytlc.bestbuy.com"];
+    if (data) {
+        tempToken = [self parseToken:data];
+    } else {
+        return nil;
+    }
+    if (tempToken) {
+        
+    }
+    return nil;
+}
+
+- (NSString*) parseToken:(NSString*)data {
+    if ([data rangeOfString:@"End Hotkey for submit"].location == NSNotFound) {
+        return nil;
+    } else {
+        NSRange end = [data rangeOfString:@"End Hotkey for submit"];
+        data = [data substringFromIndex:end.location];
+    }
+    if (([data rangeOfString:@"hidden"].location == NSNotFound) && ([data rangeOfString:@"url_login_token"].location == NSNotFound)) {
+        return nil;
+    } else {
+        NSRange beginning = [data rangeOfString:@"hidden"];
+        data = [data substringFromIndex:beginning.location + 14];
+        NSRange end = [data rangeOfString:@"url_login_token"];
+        data = [data substringToIndex:end.location - 7];
+    }
+    return data;
+}
+
+- (NSString*) getData:(NSString*)url {
+    NSURL* urlRequest = [NSURL URLWithString:url];
+    NSError* err = nil;
+    NSString* result = [NSString stringWithContentsOfURL:urlRequest encoding:NSUTF8StringEncoding error:&err];
+    if (!err) {
+        return result;
+    } else {
+        return nil;
+    }
 }
 
 - (void)viewDidLoad
